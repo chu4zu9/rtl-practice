239da6eb093aae89ed96c297c57e43d99267b0b7
chu4zu9 <uesugirennn@yahoo.co.jp>
2021-06-25 21:50:41 +0900

    add:レンダリングに関するテストを追加.

--- src/Render.test.js
+++ src/Render.test.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Render from "./Render";
-import { it } from "@jest/globals";
+import { expect } from "@jest/globals";
 
 describe("Rendering", () => {
   it("Should render all the elements correctly", () => {
     render(<Render />);
-    //screen.debug();
+    screen.getByRole("heading");
+    expect(screen.getByRole("heading")).toBeTruthy();
+    expect(screen.getByRole("textbox")).toBeTruthy();
+    expect(screen.getAllByRole("button")[0]).toBeTruthy();
+    expect(screen.getAllByRole("button")[1]).toBeTruthy();
+    expect(screen.getByText("Practice")).toBeTruthy();
+    expect(screen.queryByText("notExistText")).toBeNull();
+    expect(screen.getByTestId("copyright")).toBeTruthy();
   });
 });
