1b6e04f4ca2de8279f254aae29c2093a6f6a988f
chu4zu9 <uesugirennn@yahoo.co.jp>
2021-07-01 06:25:12 +0900

    add:incrementのテストを追加.

---
+++ src/Reducer.test.js
@@ -0,0 +1,39 @@
+import { describe, it, expect } from "@jest/globals";
+
+import reducer, {
+  increment,
+  incrementByAmount,
+  decrement,
+} from "./features/customCounter/customCounterSlice";
+
+describe("Reducer of ReduxToolKit", () => {
+  describe("increment Action", () => {
+    let initialState = {
+      mode: 0,
+      value: 1,
+    };
+    it("Should increment by 1 with mode 0", () => {
+      const action = { type: increment.type };
+      const state = reducer(initialState, action);
+      expect(state.value).toEqual(2);
+    });
+    it("Should increment by 100 with mode 1", () => {
+      initialState = {
+        mode: 1,
+        value: 1,
+      };
+      const action = { type: increment.type };
+      const state = reducer(initialState, action);
+      expect(state.value).toEqual(101);
+    });
+    it("Should increment by 10000 with mode 2", () => {
+      initialState = {
+        mode: 2,
+        value: 1,
+      };
+      const action = { type: increment.type };
+      const state = reducer(initialState, action);
+      expect(state.value).toEqual(10001);
+    });
+  });
+});
